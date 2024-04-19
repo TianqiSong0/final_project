afc234f31683eb5c09b27c0ff2e3363d54b11669
TianqiSong0 <tsong05@syr.edu>
2024-04-19 15:21:47 -0400

    Create food.py

---
+++ food.py
@@ -0,0 +1,3 @@
+#this project is designed to find the relationhip between food loss and food availability.
+#Whether is the enough amount of food had increased the chance of producing more food loss,
+#or becuase of food loss, there is a lack of food availability
