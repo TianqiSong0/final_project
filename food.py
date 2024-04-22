3192b9037c1e8c3552d55e4dfab6469ea6833ad2
TianqiSong0 <tsong05@syr.edu>
2024-04-22 10:14:11 -0400

    Update food.py

--- food.py
+++ food.py
@@ -1,3 +1,14 @@
-#this project is designed to find the relationhip between food loss and food availability.
-#Whether is the enough amount of food had increased the chance of producing more food loss,
-#or becuase of food loss, there is a lack of food availability
+#this project is designed to find people's preference over vegetable loss, and make a comparsion 
+#between vegetable loss and meat loss
+
+import pandas as pd
+fruitveg = pd.read_csv('fruitveg.csv')
+legumes = pd.read_csv('legumes.csv')
+potatoes = pd.read_csv('potatoes.csv')
+vegcan = pd.read_csv('vegcan.csv')
+vegfr = pd.read_csv('vegfr.csv')
+vegfrz = pd.read_csv('vegfrz.csv')
+vegprc = pd.read_csv('vegprc.csv')
+vegtot = pd.read_csv('vegtot.csv')
+veg = pd.read_csv('veg.csv')
+total_veg_loss = pd.DataFrame()
