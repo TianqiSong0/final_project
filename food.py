547e0c6bda42da5a93534ce738d5789328875149
TianqiSong0 <tsong05@syr.edu>
2024-04-29 12:40:46 -0400

    undone

--- food.py
+++ food.py
@@ -1,34 +1,86 @@
-#this project is designed to find people's preference over vegetable loss, and make a comparsion 
+#this project is designed to find out people's preference over vegetable loss, and make a comparsion 
 #between vegetable loss and meat loss
 
 import pandas as pd
 
-fruitveg = pd.read_excel('fruitveg.xlsx')
-legumes = pd.read_excel('legumes.xlsx', sheet_name="PccLegumes")
-PccDryBeans = pd.read_excel('legumes.xlsx', sheet_name="PccDryBeans")
-DryBeans = pd.read_excel('legumes.xlsx', sheet_name="DryBeans")
-PintoBeans = pd.read_excel('legumes.xlsx', sheet_name="PintoBeans")
-NavyBeans = pd.read_excel('legumes.xlsx', sheet_name="NavyBeans")
-GreatNorthernBeans = pd.read_excel('legumes.xlsx', sheet_name="GreatNorthernBeans")
-RedKidneyBeans = pd.read_excel('legumes.xlsx', sheet_name="RedKidneyBeans")
-DryLimaBeans = pd.read_excel('legumes.xlsx', sheet_name="DryLimaBeans")
-BlackBeans = pd.read_excel('legumes.xlsx', sheet_name="BlackBeans")
-BlackeyeBeans = pd.read_excel('legumes.xlsx', sheet_name="BlackeyeBeans")
-GarbanzoBeans = pd.read_excel('legumes.xlsx', sheet_name="GarbanzoBeans")
-SmallWhiteBeans = pd.read_excel('legumes.xlsx', sheet_name="SmallWhiteBeans")
-SmallRedBeans = pd.read_excel('legumes.xlsx', sheet_name="SmallRedBeans")
-PinkBeans = pd.read_excel('legumes.xlsx', sheet_name="PinkBeans")
-CranAndMiscbeans = pd.read_excel('legumes.xlsx', sheet_name="CranAndMiscbeans")
-potatoes = pd.read_excel('potatoes.xlsx', sheet_name="Pcc")
-SweetPotatoes = pd.read_excel('potatoes.xlsx', sheet_name="SweetPotatoes")
+#read the files and convert sheets into dictionaries
+filelist = ['legumes.xlsx', 'potatoes.xlsx', 'vegcan.xlsx', 'vegfr.xlsx', 'vegfrz.xlsx', 'vegprc.xlsx', 'vegtot.xlsx', 'veg.xlsx']
+for f in filelist:
+    fh = pd.read_excel(f,sheet_name=None)
+print(fh)
+
+#%%
+#create dataframes by using dictionaries' keys
 
 
-vegcan = pd.read_excel('vegcan.xlsx')
-vegfr = pd.read_excel('vegfr.xlsx')
-vegfrz = pd.read_excel('vegfrz.xlsx')
-vegprc = pd.read_excel('vegprc.xlsx')
-vegtot = pd.read_excel('vegtot.xlsx')
-veg = pd.read_excel('veg.xlsx')
-total_veg_loss = pd.concat([fruitveg, legumes, potatoes, vegcan, vegfr, vegfrz, vegprc, vegtot, veg])
+
+df = {}
+for k in fh.keys():
+    subdf = pd.DataFrame({k: fh["key", "value"]})
+    
+    
+    
+    #%%
+    subdf = pd.DataFrame({fh: fh[k]})
+
+
+#%%
+for k in fh:
+    df = pd.DataFrame(fh.keys)
+    
+#%%  
+legumes = pd.DataFrame()
+
+
+def Df(filename):
+    df = pd.DataFrame(filename)
+    return df
+
+
+    for value, i in fh:
+        df = pd.DataFrame([value], index=[0])
+        #df = df.append(i, ignore_index=True)
+              
+#%%       
+        
+    subdf = pd.DataFrame(fh)
+    df = df.extend(subdf) 
+    
+#%%
+
+for f in file_list:
+    fh = pd.read_excel(f, sheet_name=None)
+    
+
+# Optionally, reset the index of the DataFrame after appending
+df.reset_index(drop=True, inplace=True)
+
+#%%
+legumes = rexcel('legumes.xlsx')
+potatoes = rexcel('potatoes.xlsx')
+vegcan = rexcel('vegcan.xlsx')
+vegfr = rexcel('vegfr.xlsx')
+vegfrz = rexcel('vegfrz.xlsx')
+vegprc = rexcel('vegprc.xlsx')
+vegtot = rexcel('vegtot.xlsx')
+veg = rexcel('veg.xlsx')
+
+#%%
+legume = pd.DataFrame(legumes)
+potato = pd.DataFrame(potatoes)
+
+colloc = [0]
+colname = ["year"]
+
+legumes.raname(columns=colname)
+
+# Assuming 'legumes' is your DataFrame
+col_index = 0  # Index of the column you want to rename
+new_column_name = "year"  # New name for the column
+
+# Rename the specific column using iloc and rename
+legumes.rename(columns={legumes.columns[col_index]: new_column_name}, inplace=True)
+#%%
+total_veg_loss = pd.concat([legumes, potatoes, vegcan, vegfr, vegfrz, vegprc, vegtot, veg])
 #%%
 total_veg_loss = total_veg_loss.merge()
